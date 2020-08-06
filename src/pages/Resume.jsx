496f81883130e11586bbec97578d99c8dfddbf8a
Dot <arwin.mavaji.1995+git@gmail.com>
2020-08-06 20:42:17 +0200

    placeholder pdf

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 export default function Resume() {
   return (
     <div className='resume'>
-      <object src='Template.pdf' type='application/pdf'>
-        <embed src='Template.pdf' type='application/pdf' />
+      <object
+        src='https://www.tristanwhite.com.au/wp-content/uploads/2014/10/pdf-placeholder.pdf'
+        type='application/pdf'>
+        <embed
+          src='https://www.tristanwhite.com.au/wp-content/uploads/2014/10/pdf-placeholder.pdf'
+          type='application/pdf'
+        />
       </object>
     </div>
   );
